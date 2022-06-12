8b8463391e12946eea1af5f70abcf9a7e386932e
barnes <beth.m.barnes@gmail.com>
2022-06-11 18:53:17 -0700

    add settings buttons

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
 
 
-function api_call(text) {
-  // send text to OpenAI API using fetch
-  const url = "https://api.openai.com/v1/engines/rust/completion";
-  const params = {
-    "engine": "davinci-text-002",
-    "text": text,
-    "max_choices": 10,
-    "temperature": 0.8,
+
+function api_call(text, temp = 0, n_tokens = 50) {
+  // send text, temperature to Flask backend
+  const data = { "text": text, "temp": temp, "n_tokens": n_tokens }
+  const headers = { 'Content-Type': 'application/json' }
+  const args = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data)
   }
+  console.log('fetching')
+  const completion = fetch('/submit_prompt', args).then(
+    function (completion) {
+      console.log('fetching done')
 
-  const response = fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + process.env.REACT_APP_OPENAI_API_KEY,
-    },
-    body: JSON.stringify(params),
-  });
+      return completion.text()
 
-
-  return " API completion";
+    }
+  )
+  return completion;
+  // fetch('/submit_prompt')
 }
 
-const Prompt_Window = () => {
+
+const PromptWindow = () => {
 
   const [text, setText] = useState('');
+  const [temp, setTemp] = useState(0);
+  const [n_tokens, setNTokens] = useState(50);
   function get_completion(text) {
     // send text to OpenAI API
-    var completion = api_call(text);
-    setText(text + completion);
+    api_call(text, temp, n_tokens).then(completion => setText(text + completion));
     return
   }
 
@@ -41,8 +42,50 @@ const Prompt_Window = () => {
     <div>
       <textarea rows="20" value={text} onChange={(e) => setText(e.target.value)} />
       <br></br>
-      <button id="submit_prompt" onClick={() => get_completion(text)}>submit</button>
+      <button id="submit_prompt" onClick={() => get_completion(text, temp, n_tokens)}>submit</button>
     </div>
+  )
+}
+const Logs = () => {
+  const [logs, setLogs] = useState('');
+
+  return (
+    <tr>
+      <td>
+        logs
+      </td>
+    </tr>
+  );
+}
+const Temp = () => {
+  const [temp, setTemp] = useState(0);
+
+  return (
+    <div>
+      <input type="number" value={temp} onChange={(e) => setTemp(e.target.value)} />
+      <label htmlFor="temp">Temp</label>
+    </div>
+  );
+}
+const NTokens = () => {
+  const [n_tokens, setNTokens] = useState(50);
+
+  return (
+    <div>
+      <input type="number" value={n_tokens} onChange={(e) => setNTokens(e.target.value)} />
+      <label htmlFor="n_tokens">NTokens</label>
+    </div>
+  );
+}
+
+const ChangeNewlines = () => {
+  const [newlines, setNewlines] = useState(false);
+  return (
+    <div>
+      <input type="checkbox" value={newlines} onChange={(e) => setNewlines(e.target.value)} />
+      <label htmlFor="newlines">Show Newlines</label>
+    </div>
+
   )
 }
 
@@ -53,21 +96,54 @@ function get_completion_on_ctrl_enter(e) {
 
   }
 }
-function App() {
-  useEffect(() => {
-    axios.get('http://localhost:5002/flask/hello').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
 
-  }, [])
+function App() {
   return (
-    <div onKeyDown={get_completion_on_ctrl_enter}>
-      <Prompt_Window />
-    </div>
+    <div>
+      <div className="settings_bar">
+        <a className='setting'>
+          <Temp />
+        </a>
+        <a className='setting'>
+          <NTokens />
+        </a>
+        <a className='setting'>
+          <ChangeNewlines />
+        </a>
+
+      </div>
+      <div onKeyDown={get_completion_on_ctrl_enter}>
+        <PromptWindow />
+      </div>
+      <div className="container">
+        <table className="logs">
+          <thead>
+            <tr className="table-header">
+
+              <td>
+                Prompt
+              </td>
+              <td>
+                Completion
+              </td>
+              <td>
+                _
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            <Logs />
+          </tbody>
+        </table>
+      </div>
+
+
+
+
+
+    </div >
+
   );
 }
-
 export default App;
+
--- src/server.py
+++ src/server.py
@@ -1,74 +1,14 @@
 from sqlite3 import Timestamp
+from click import prompt
 import flask
 import json
 from flask import Flask, render_template, Markup
 import os
 import argparse
-from templates import *
 import openai
 import time
 
 app = Flask(__name__)
-
-
-class State:
-    def __init__(self, n_tokens=50, text="", temp=0, show_newlines=True):
-        self.n_tokens = int(n_tokens)
-        self.text = text
-        self.temp = int(temp)
-        self.show_newlines = show_newlines
-
-    def make_html(self):
-        textbox_html = edit_form_template.format(current_text=self.text)
-        n_tokens_html = n_tokens_template.format(n_tokens=self.n_tokens)
-        temp_html = temp_template.format(temp=self.temp, display_temp=self.temp / 100)
-        change_newlines_html = change_newlines_template.format(
-            checked="checked" if self.show_newlines else ""
-        )
-        logs = self.make_log_html()
-        html = render_template(
-            "test.html",
-            textbox=textbox_html,
-            n_tokens=n_tokens_html,
-            change_newlines=change_newlines_html,
-            temp=temp_html,
-            logs=logs,
-        )
-        return html
-
-    def make_log_html(self):
-        print(self.show_newlines)
-        with open("log.txt", "r") as f:
-            data = f.read()
-        # display logs nicely in browser
-        logs = []
-        for line in data.split("\n")[-20:]:
-            if line:
-                line = json.loads(line)
-                checkbox = remove_row_template.format(
-                    id=line["time_id"],
-                )
-                save = save_button_template.format(
-                    id=line["time_id"],
-                )
-                # make sure newlines are preserved in html
-                if self.show_newlines:
-                    line["prompt"] = line["prompt"].replace("\n", "<br>")
-                    line["completion"] = line["completion"].replace("\n", "<br>")
-
-                logs.append(
-                    log_template.format(
-                        display_temp=line["temp"] / 100,
-                        prompt=line["prompt"],
-                        completion=line["completion"],
-                        checkbox=checkbox,
-                        save=save,
-                    )
-                )
-
-        # reverse
-        logs.reverse()
-        return Markup("\n".join(logs))
 
 
 def archive(id):
@@ -128,21 +68,27 @@ def root():
     return state.make_html()
 
 
-@app.route("/submit_prompt")
+@app.route("/submit_prompt", methods=["POST"])
 def submit_prompt():
-    prompt = flask.request.args["text"]
-    temp = float(flask.request.args["temp"])
+    print("submit_prompt")
+    # get json from request
+    data = flask.request.get_json()
+    print(data)
+    prompt = data["text"]
+    temp = data["temp"]
+    n_tokens = data["n_tokens"]
     # send prompt to openai
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=int(state.n_tokens),
+        max_tokens=n_tokens,
         temperature=temp,
         logprobs=1,
     )
     completion = response.choices[0].text
     logprobs = response.choices[0].logprobs
-    log_to_file(prompt=prompt, completion=completion, logprobs=logprobs)
+    print(completion)
+    # log_to_file(prompt=prompt, completion=completion, logprobs=logprobs))
     return completion
 
 
@@ -186,14 +132,13 @@ if __name__ == "__main__":
     parser.add_argument("--public", action="store_true")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
-    port = int(os.environ.get("PORT", 5003))
+    port = int(os.environ.get("PORT", 5004))
     aws = True
     push_to_aws = True
     if args.debug:
         app.debug = True
         push_to_aws = False
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    state = State()
     app.run(
         host="0.0.0.0" if args.public else "127.0.0.1",
         port=port,
