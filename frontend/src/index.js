72c4a8aba2fd325327017e10dad77353b259397f
Tamarheron <beth.m.barnes@gmail.com>
2022-07-11 11:08:34 -0700

    auth0 stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { Auth0Provider } from "@auth0/auth0-react";
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.AUTH0_DOMAIN}
+    clientId={process.env.AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
